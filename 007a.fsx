89e5b04bec24ef44eadb9457b6789803998795cf
mg <108034506+mg0x7BE@users.noreply.github.com>
2025-11-18 00:39:14 +0100

    exercise

--- 007a.fsx
+++ 007a.fsx
@@ -1,7 +1,18 @@
-﻿let result =
-    [2; 3; 4]
+﻿let list = [2; 3; 4]
+
+let result1 =
+    list
     |> List.map (fun x -> [1 .. x])
     |> List.collect (fun x -> x)
     |> List.filter (fun x -> x % 2 = 0)
     
-printfn $"%A{result}"
+printfn $"%A{result1}"
+
+// alternative:
+
+let result2 =
+    list
+    |> List.collect (fun x -> [1 .. x])  // map + flatten in one step
+    |> List.filter (fun x -> x % 2 = 0)
+    
+printfn $"%A{result2}"
